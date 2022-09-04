1311485da33868a3aa2fddd827986d2f0c71b2c3
eennugraha <nugraha.hendrisatya@gmail.com>
2022-09-04 13:06:15 +0700

    user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,7 @@ const userRoute = require("express").Router();
 const UserController = require("../controllers/UserController");
 
 userRoute.get("/", UserController.getAllUsers);
-userRoute.post("/register", UserController.create);
+userRoute.post("/tes", UserController.create);
 userRoute.post("/login", UserController.login);
 userRoute.put("/:id", UserController.update);
 userRoute.delete("/:id", UserController.delete);
